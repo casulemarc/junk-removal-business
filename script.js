4628204f47cc6089960dac153fbe48ac15f3cc97
casulemarc <casulmarc2014@gmail.com>
2021-08-21 04:55:14 -0400

    input validation

--- script.js
+++ script.js
@@ -10,17 +10,33 @@ callBtn.addEventListener("click", function () {
 const SendQuoteBtn = document.querySelector("#submit");
 SendQuoteBtn.addEventListener("click", function () {
   //input validation
-  let inputValues = document.querySelectorAll(".form-control").value;
+  let inputValues_1 = document.querySelector(".form-control-1");
+  let inputValues_2 = document.querySelector(".form-control-2");
+  let inputValues_3 = document.querySelector(".form-control-3");
 
   //check if not null or empty
-  if (inputValues !== "") {
+  if (
+    inputValues_1.value === "" &&
+    inputValues_2.value === "" &&
+    inputValues_3.value === ""
+  ) {
+    //show error message
+    const namesError = "Names Missing";
+    const emailError = "Email Missing";
+    const msgError = "Message Missing";
+
+    inputValues_1.value = namesError;
+    inputValues_2.value = emailError;
+    inputValues_3.value = msgError;
+  } else {
     SendQuoteBtn.textContent = "Sending...";
     //set a timer to reset button text after 3 seconds
     setTimeout(function () {
+      //reset input values
+      inputValues_1.value = "";
+      inputValues_2.value = "";
+      inputValues_3.value = "";
       SendQuoteBtn.textContent = "Message Sent😊";
     }, 5000);
-  } else {
-    //show error message
-    inputValues = "Hello Missing";
   }
 });
