9f7f325c7d1be7dad6b8a10685c134a49f81e131
casulemarc <casulmarc2014@gmail.com>
2021-08-22 02:18:42 -0400

    Error messsage added

--- script.js
+++ script.js
@@ -14,21 +14,42 @@ SendQuoteBtn.addEventListener("click", function () {
   let inputValues_2 = document.querySelector(".form-control-2");
   let inputValues_3 = document.querySelector(".form-control-3");
 
+  //show error message
+  const namesError = "Names Missing";
+  const emailError = "Email Missing";
+  const msgError = "Message Missing";
+
   //check if not null or empty
   if (
-    inputValues_1.value === "" &&
-    inputValues_2.value === "" &&
-    inputValues_3.value === ""
+    (inputValues_1.value === "" &&
+      inputValues_2.value === "" &&
+      inputValues_3.value === "") ||
+    (inputValues_1.value === namesError &&
+      inputValues_2.value === emailError &&
+      inputValues_3.value === msgError)
   ) {
     //show error message
-    const namesError = "Names Missing";
-    const emailError = "Email Missing";
-    const msgError = "Message Missing";
-
+    inputValues_1.style.color = "rgb(252, 103, 103)";
+    inputValues_2.style.color = "rgb(252, 103, 103)";
+    inputValues_3.style.color = "rgb(252, 103, 103)";
     inputValues_1.value = namesError;
     inputValues_2.value = emailError;
     inputValues_3.value = msgError;
+
+    //set a timer to reset button text after 3 seconds
+    setTimeout(function () {
+      //reset input values
+      inputValues_1.value = "";
+      inputValues_2.value = "";
+      inputValues_3.value = "";
+    }, 2000);
   } else {
+    if (
+      inputValues_1.value !== "" &&
+      inputValues_2.value !== "" &&
+      inputValues_3.value !== ""
+    ) {
+    }
     SendQuoteBtn.textContent = "Sending...";
     //set a timer to reset button text after 3 seconds
     setTimeout(function () {
